8df6bfab82d8c1884066fa0e00a6b142bf0c247a
Lee Bock <lbock@esri.com>
2020-10-12 11:46:44 -0400

    added the ability to loop through multiple configs

--- driver.js
+++ driver.js
@@ -1,70 +1,81 @@
 const child_process = require("child_process");
 
-const CONFIG = {
-    species: "Pinus contorta",
-    csv: "../data/pinus-contorta.csv",
-    service: "https://services.arcgis.com/nzS0F0zdNLvs7nc8/ArcGIS/rest/services/pinus_contorta_edit/FeatureServer/0"
-};
+const CONFIGS = [
+    {
+        species: "Pinus contorta",
+        csv: "../data/pinus-contorta.csv",
+        service: "https://services.arcgis.com/nzS0F0zdNLvs7nc8/ArcGIS/rest/services/pinus_contorta_edit/FeatureServer/0"
+    },
+    {
+        species: "Acer spicatum",
+        csv: "../data/acer-spicatum.csv",
+        service: "https://services.arcgis.com/nzS0F0zdNLvs7nc8/arcgis/rest/services/acer_spicatum_edit/FeatureServer/0"
+    }    
+];
 
-console.log();
+CONFIGS.forEach((config, i) => {
 
-if (
-    child_process.spawnSync(
-        "node",
-        ["inat-fetch", CONFIG.species, CONFIG.csv],
-        {stdio: "inherit"}
+    console.log();
+    
+    if (
+        child_process.spawnSync(
+            "node",
+            ["inat-fetch", config.species, config.csv],
+            {stdio: "inherit"}
+        )
+        .status !== 0
     )
-    .status !== 0
-)
-{
-    process.exit();
-}
+    {
+        process.exit();
+    }
 
-console.log();
+    console.log();
+    
+    if (
+        child_process.spawnSync(
+            "node",
+            ["project-coords", config.csv, "temp-wm.csv"],
+            {stdio: "inherit"}
+        )
+        .status !== 0    
+    ) 
+    {
+        process.exit();
+    }
 
-if (
-    child_process.spawnSync(
-        "node",
-        ["project-coords", CONFIG.csv, "temp-wm.csv"],
-        {stdio: "inherit"}
+    console.log();
+    
+    if (
+        child_process.spawnSync(
+            "node",
+            [
+                "fs-purge", 
+                config.service
+            ],
+            {stdio: "inherit"}
+        )
+        .status !== 0    
     )
-    .status !== 0    
-) 
-{
-    process.exit();
-}
+    {
+        process.exit();
+    }
+    
+    console.log();
 
-console.log();
-
-if (
-    child_process.spawnSync(
-        "node",
-        [
-            "fs-purge", 
-            CONFIG.service
-        ],
-        {stdio: "inherit"}
+    if (
+        child_process.spawnSync(
+            "node",
+            [
+                "fs-load", 
+                "temp-wm.csv",
+                config.service
+            ],
+            {stdio: "inherit"}
+        )
+        .status !== 0    
     )
-    .status !== 0    
-)
-{
-    process.exit();
-}
-
-console.log();
-
-if (
-    child_process.spawnSync(
-        "node",
-        [
-            "fs-load", 
-            "temp-wm.csv",
-            CONFIG.service
-        ],
-        {stdio: "inherit"}
-    )
-    .status !== 0    
-)
-{
-    process.exit();
-}
+    {
+        process.exit();
+    }
+    
+});
