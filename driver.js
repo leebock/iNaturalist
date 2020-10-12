65fbc916fa0ff7c0dd9514b21860b6aae08f4a9c
Lee Bock <lbock@esri.com>
2020-10-12 11:02:16 -0400

    Created a config structure to hold all the relevant information pieces for extracting a species from iNatural and inserting into ArcGIS.com.  Note that the call to inat-fetch was altered to include species as argument

--- driver.js
+++ driver.js
@@ -1,11 +1,17 @@
 const child_process = require("child_process");
 
+const CONFIG = {
+    species: "Pinus contorta",
+    csv: "../data/pinus-contorta.csv",
+    service: "https://services.arcgis.com/nzS0F0zdNLvs7nc8/ArcGIS/rest/services/pinus_contorta_edit/FeatureServer/0"
+};
+
 console.log();
 
 if (
     child_process.spawnSync(
         "node",
-        ["inat-fetch", "../data/pinus-contorta.csv"],
+        ["inat-fetch", CONFIG.species, CONFIG.csv],
         {stdio: "inherit"}
     )
     .status !== 0
@@ -19,7 +25,7 @@ console.log();
 if (
     child_process.spawnSync(
         "node",
-        ["project-coords", "../data/pinus-contorta.csv", "temp-wm.csv"],
+        ["project-coords", CONFIG.csv, "temp-wm.csv"],
         {stdio: "inherit"}
     )
     .status !== 0    
@@ -35,7 +41,7 @@ if (
         "node",
         [
             "fs-purge", 
-            "https://services.arcgis.com/nzS0F0zdNLvs7nc8/ArcGIS/rest/services/pinus_contorta_edit/FeatureServer/0"
+            CONFIG.service
         ],
         {stdio: "inherit"}
     )
@@ -53,7 +59,7 @@ if (
         [
             "fs-load", 
             "temp-wm.csv",
-            "https://services.arcgis.com/nzS0F0zdNLvs7nc8/ArcGIS/rest/services/pinus_contorta_edit/FeatureServer/0"
+            CONFIG.service
         ],
         {stdio: "inherit"}
     )
