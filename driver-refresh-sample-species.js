47a8450f12f28f700232027bdaf1f81574c90232
Lee Bock <lbock@esri.com>
2020-10-20 10:27:19 -0400

    updated paths to reflect new data sub-folder structure

--- driver-refresh-sample-species.js
+++ driver-refresh-sample-species.js
@@ -3,12 +3,12 @@ const child_process = require("child_process");
 const CONFIGS = [
     {
         species: "Pinus contorta",
-        csv: "../data/pinus-contorta.csv",
+        csv: "../data/inat-sample-pulls/pinus-contorta.csv",
         service: "https://services.arcgis.com/nzS0F0zdNLvs7nc8/ArcGIS/rest/services/pinus_contorta_edit/FeatureServer/0"
     },
     {
         species: "Acer spicatum",
-        csv: "../data/acer-spicatum.csv",
+        csv: "../data/inat-sample-pulls/acer-spicatum.csv",
         service: "https://services.arcgis.com/nzS0F0zdNLvs7nc8/arcgis/rest/services/acer_spicatum_edit/FeatureServer/0"
     }    
 ];
