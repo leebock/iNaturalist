41d48cad31e00733228a1f9c212a8ee49b39e6d1
Lee Bock <lbock@esri.com>
2020-10-27 17:59:28 -0400

    snapshot.js: adding optional args for sort field, output fields, and token file

--- fs-snapshot.js
+++ fs-snapshot.js
@@ -3,15 +3,21 @@
     "use strict";
 
     const fetch = require('node-fetch');
-
-    if (process.argv.length !== 4) {
-    	console.log("Usage: " + __filename + " <service> <output_file>");
+    const fs = require("fs");
+    
+    if (process.argv.length < 4) {
+    	console.log("Usage: " + __filename + " service output_file [sort_field] [output_fields] [token_file]");
     	process.exit(-1);
     }
-
+    
     const SERVICE = process.argv[2];
     const OUTPUT_FILE = process.argv[3];
-
+    const SORT_FIELD = process.argv.length > 3 ? process.argv[4].trim() : null;
+    const OUTPUT_FIELDS = process.argv.length > 4 ? process.argv[5].trim() : null;
+    const TOKEN = process.argv.length > 5 ? 
+        JSON.parse(fs.readFileSync(process.argv[6])).token : 
+        null;
+        
     console.log("----------------------------------------------------");
     console.log("Making snapshot from feature service...");
     console.log("Service: "+SERVICE);
@@ -34,7 +40,7 @@
     /* write to csv output file */
     require("fs").writeFile(
         OUTPUT_FILE, 
-        require('json2csv').parse(_results), 
+        require('json2csv').parse(_results, OUTPUT_FIELDS && OUTPUT_FIELDS.length > 0 ? {fields: OUTPUT_FIELDS.split(",")} : {}), 
         (err) => {
             if (err) {
                 throw("error writing file!");
@@ -51,17 +57,25 @@
         const response = await fetch(
             SERVICE+"/query"+
             "?where="+encodeURIComponent("1=1")+
+            (SORT_FIELD ? "&orderByFields="+SORT_FIELD : "")+
             "&resultOffset="+offset+
             "&resultRecordCount=2000"+            
             "&outFields=*"+
-            "&f=pjson"
+            "&f=pjson"+
+            (TOKEN ? "&token="+TOKEN : "")
         );
         const json = await response.json();
         return json.features.map(function(value){return value.attributes;});
     }
 
     async function getCount()  {
-        const response = await fetch(SERVICE+"/query?where="+encodeURIComponent("1=1")+"&returnCountOnly=true&f=pjson");
+        const response = await fetch(
+            SERVICE+"/query"+
+            "?where="+encodeURIComponent("1=1")+
+            "&returnCountOnly=true"+
+            "&f=pjson"+
+            (TOKEN ? "&token="+TOKEN : "")
+        );
         const json = await response.json();
         return json.count;
     }
