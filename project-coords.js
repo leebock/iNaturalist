bc07fc7e17d40a8ac3bc0a0f667a757b21b59e7b
leebock <leebockmail@gmail.com>
2021-04-20 17:29:23 -0400

    updated project-coords.js to use while and async

--- project-coords.js
+++ project-coords.js
@@ -1,107 +1,74 @@
-const http = require("http");
-const querystring = require("querystring");
+(async () => {
 
-const GEOMETRY_SERVICE = "http://sampleserver6.arcgisonline.com/arcgis/rest/services/Utilities/Geometry/GeometryServer";
+	const fetch = require("node-fetch");
+	const querystring = require("querystring");
 
-if (process.argv.length !== 4) {
-	console.log("Usage: " + __filename + " input_file output_file");
-	process.exit(-1);
-}
+	const GEOMETRY_SERVICE = "http://sampleserver6.arcgisonline.com/arcgis/rest/services/Utilities/Geometry/GeometryServer";
 
-const IN_FILE = process.argv[2];
-const OUT_FILE = process.argv[3];
+	if (process.argv.length !== 4) {
+		console.log("Usage: " + __filename + " input_file output_file");
+		process.exit(-1);
+	}
 
-console.log("----------------------------------------------------");
-console.log("Projecting data points to web mercator...");
-console.log("Input file: "+IN_FILE);
-console.log("Output file: "+OUT_FILE);
+	const IN_FILE = process.argv[2];
+	const OUT_FILE = process.argv[3];
 
-var _records = [];
-var _counter = 0;
+	console.log("----------------------------------------------------");
+	console.log("Projecting data points to web mercator...");
+	console.log("Input file: "+IN_FILE);
+	console.log("Output file: "+OUT_FILE);
 
-require('csvtojson')()
-	.fromFile(IN_FILE)
-	.on('data',(data)=>{
-		//data is a buffer object
-        _records.push(JSON.parse(data.toString('utf8')));
-	})
-	.on(
-		"done", 
-		function() {
-			console.log("Processing", _records.length, "records.");
-            doIt();
-		}
-	);
-    
-function doIt()
-{
-    var record = _records[_counter];
-    project(
-        record.lon, record.lat, 
-        function(obj) {
-			process.stdout.clearLine();  // clear current text
-			process.stdout.cursorTo(0);  // move cursor to beginning of line
-			process.stdout.write("Progress: "+parseInt(( (_counter+1) / _records.length)*100)+"%");
-            record.x = obj.x;
-            record.y = obj.y;
-            _counter++;
-            if (_counter < _records.length) {
-                doIt();
-            } else {
-                finish();
-				console.log("");
-				console.log("Success!");
-				console.log("----------------------------------------------------");
-            }
-        }
-    );
-}
+	const _records = await require('csvtojson')().fromFile(IN_FILE);
+	console.log("Processing", _records.length, "records.");
+	
+	var _counter = 0;
+	
+	while (_counter < _records.length) {
+		const record = _records[_counter];
+		const obj = await project(record.lon, record.lat);
+		process.stdout.clearLine();  // clear current text
+		process.stdout.cursorTo(0);  // move cursor to beginning of line
+		process.stdout.write("Progress: "+parseInt(( (_counter+1) / _records.length)*100)+"%");
+		record.x = obj.x;
+		record.y = obj.y;
+		_counter++;
+	}
 
-function finish() 
-{
-    var csv = require("json2csv").parse(_records); /* json2csv */
+	const csv = require("json2csv").parse(_records); /* json2csv */
 	require("fs").writeFile(OUT_FILE, csv, () => {}); /* writeFile */    
-}
 
-function project(x, y, callBack)
-{
+	console.log("");
+	console.log("Success!");
+	console.log("----------------------------------------------------");
 
-	var postData = {
-		inSR:4326,
-		outSR:3857,
-		geometries: JSON.stringify({
-			"geometryType":"esriGeometryPoint",
-			"geometries":[{x:x,y:y}]
-		}),
-		f:"pjson"
-	}; 
+	async function project(x, y)
+	{
+		const postData = querystring.stringify(
+			{
+				inSR:4326,
+				outSR:3857,
+				geometries: JSON.stringify({
+					"geometryType":"esriGeometryPoint",
+					"geometries":[{x:x,y:y}]
+				}),
+				f:"pjson"
+			}			
+		);
+		const response = await fetch(
+			GEOMETRY_SERVICE+"/project",
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/x-www-form-urlencoded",
+					"Content-Length": postData.length
+				},
+				body: postData
+			}			
+		)
+		const json = await response.json();
+		return json.geometries[0];
+	}
 
-	postData = querystring.stringify(postData);
-
-	var options = {
-		host: "sampleserver6.arcgisonline.com",
-		method: "POST",
-		port: 80,
-		path: GEOMETRY_SERVICE.replace("http://sampleserver6.arcgisonline.com","")+"/project",
-		headers:{"Content-Type": "application/x-www-form-urlencoded","Content-Length": postData.length}
-	};
-
-	var result = "";	
-	
-	var req = http.request(options, function(res) {
-		res.setEncoding('utf8');
-		res.on('data', function (chunk) {
-			result = result+chunk;
-		}).on('end', function(){
-			callBack(JSON.parse(result).geometries[0]);
-		});
-	});
-	
-	req.on('error', function(e) {
-		console.log('problem with request: ' + e.message);
-	});
-	
-	req.write(postData);
-	req.end();
-
-}
+})().catch(err => {
+    console.error(err);
+});
